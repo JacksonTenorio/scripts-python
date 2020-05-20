4b76ee9030e1bb8066148a011cdb3608d128d4e5
Jackson <jacksontenorio8@gmail.com>
2020-05-20 19:23:58 -0300

    Enem
    Programa que calcula a média do Enem.

---
+++ Python/ENEM.py
@@ -0,0 +1,7 @@
+n1 = float(input('Redação:'))
+n2 = float(input('Ciências da Natureza e suas Tecnologias:'))
+n3 = float(input('Ciências Humanas e suas Tecnologias:'))
+n4 = float(input('Linguagens, Códigos e suas Tecnologias:'))
+n5 = float(input('Matemática e suas Tecnologias:'))
+x = (n1 + n2 + n3 + n4 + n5) / 5
+print('Sua média no Enem é: {}'.format(x))
