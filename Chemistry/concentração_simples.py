30b40c0f1fe9a37f8e029546717c7d940cf9c120
Jackson <jacksontenorio8@gmail.com>
2020-07-24 08:46:34 -0300

    concentração simples
    Programa que calcula a concentração simlpes de uma solução.

---
+++ Chemistry/concentração_simples.py
@@ -0,0 +1,5 @@
+# concentração, em g/l
+m1 = float(input('Massa do soluto em gramas: '))
+v = float(input('Volume da solução em litro: '))
+c = m1 / v
+print('A concentração comum ou simples da solução é: {:.2f} g/l'.format(c))
